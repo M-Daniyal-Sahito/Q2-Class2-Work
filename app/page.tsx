829fae0eb3eb6544526dacf81b0da8114cf88db3
M Daniyal Sahito <princedaniyal12222222@gmail.com>
2024-09-19 21:51:16 +0500

    Remove unwanted text

--- app/page.tsx
+++ app/page.tsx
@@ -14,12 +14,4 @@ export default function Home() {
     </>
   );
 }
-// 🚀 Excited to Share My Progress in Next.js! 🚀
 
-// I’ve just completed my Class 2 Assignment in Next.js, where I built a website template that includes a fully functional navbar, content area, and footer. 🎉
-
-// This project helped me dive deeper into component-based architecture, and I’m really enjoying learning how to structure web applications efficiently. Looking forward to building more with Next.js!
-
-// Feel free to check out my journey, and let's connect! 💻👨‍💻
-
-// #Nextjs #WebDevelopment #Frontend #LearningJourney #HTML #CSS #React #JavaScript #OpenToOpportunitie
